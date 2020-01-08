9703fb8a01479d1bb02fe65db9632bd0012bd5b3
František Gič <mcfreddie777@gmail.com>
2020-01-08 12:16:34 +0100

    Override tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
     theme: {
         extend: {
+            colors: {
+                yellow: {
+                    100: '#ecd47f',
+                    200: '#e1ba32',
+                    300: '#ddb119',
+                    400: '#FFED4A',
+                    500: '#daa900',
+                    700: '#c49800',
+                    800: '#684F1D',
+                    900: '#453411'
+                }
+            },
             width: {
                 '96': '24rem'
             }
