3b95f3accf4de01f69e88b3edb4f2d92fac190d8
František Gič <mcfreddie777@gmail.com>
2020-01-08 20:36:12 +0100

    Implement collapsible sidebar

--- resources/js/components/Navigation/NavigationItem.vue
+++ resources/js/components/Navigation/NavigationItem.vue
@@ -1,11 +1,20 @@
 <template>
-    <div class="flex flex-col">
+    <div
+        class="flex flex-col"
+        :data-collapsed="collapsed"
+    >
         <router-link
             :to="item.path"
-            class="flex items-center px-6 py-2 hover:bg-gray-700 text-gray-600 hover:text-gray-500"
+            class="flex items-center justify-center py-2 hover:bg-gray-700 text-gray-600 hover:text-gray-500"
+            :class="collapsed ? 'flex-col h-24 justify-center py-3' : 'px-6 '"
         >
-            <i :class="item.icon" class="w-1/6"/>
-            <span class="text-md pl-1 w-5/6">{{ item.title }}</span>
+            <i
+                :class="[item.icon,collapsed ? 'w-full text-center text-2xl' : 'w-1/6']"
+            />
+            <span
+                class="text-md w-5/6"
+                :class="collapsed ? 'w-full text-center mt-2 px-1' : 'pl-1'"
+            >{{ item.title }}</span>
         </router-link>
     </div>
 </template>
@@ -14,9 +23,18 @@
     export default {
         name: "NavigationItem",
 
-        props: [
-            'item',
-        ],
+        props: {
+            'item': {
+                type: Object,
+                required: true,
+            }
+        },
+
+        computed: {
+            collapsed: function () {
+                return this.$store.getters['App/isNavigationCollapsed'];
+            },
+        },
     }
 </script>
 
@@ -28,6 +46,11 @@
         }
 
         //noinspection CssInvalidAtRule
-        @apply .bg-gray-700 .border-r-4 .border-yellow-500 .text-gray-500;
+        @apply .bg-gray-700 .text-gray-500;
+    }
+
+    :not([data-collapsed]) > .router-link-active {
+        //noinspection CssInvalidAtRule
+        @apply .border-r-4 .border-yellow-500 ;
     }
 </style>
--- resources/js/components/Navigation/NavigationItemGroup.vue
+++ resources/js/components/Navigation/NavigationItemGroup.vue
@@ -1,34 +1,38 @@
 <template>
-    <div
-        class="mt-10"
-    >
-        <p class="uppercase text-sm text-gray-500 font-bold mb-2 pl-6">{{ group.title }}</p>
+<!--        :class="!collapsed ? 'mt-10' : ''"-->
+    <div>
+        <p
+            class="uppercase text-sm text-gray-500 font-bold mb-2 pl-6"
+            :class="collapsed ? 'hidden':''"
+        >{{ group.title }}</p>
         <NavigationItem
             v-for="(item,index) in group.items"
-            v-bind:key="index"
+            :key="index"
             :item="item"
         />
     </div>
 </template>
 
 <script>
-
     import NavigationItem from "./NavigationItem";
 
     export default {
         name: "NavigationItemGroup",
 
-        props: [
-            'group',
-        ],
-
+        props: {
+            'group': {
+                type: Object,
+                required: true
+            }
+        },
         components: {
             NavigationItem
         },
 
+        computed: {
+            collapsed: function () {
+                return this.$store.getters['App/isNavigationCollapsed'];
+            },
+        },
     }
 </script>
-
-<style scoped>
-
-</style>
