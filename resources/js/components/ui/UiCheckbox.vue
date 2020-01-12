deda089feebe7a719ced3e8077532dff8eeed6a0
František Gič <mcfreddie777@gmail.com>
2020-01-12 17:02:15 +0100

    Implement UI components

---
+++ resources/js/components/ui/UiCheckbox.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <label
+            v-if="labeled"
+            :for="name"
+            @click.prevent="toggle"
+        >
+            {{ name }}
+        </label>
+        <div
+            class="w-4 h-4 bg-yellow-500 rounded flex items-center justify-center"
+            @click="toggle"
+            ref=""
+        >
+            <i
+                class="fas fa-check text-white text-xs"
+                v-if="checked"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ui-checkbox",
+
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            checked: {
+                type: Boolean,
+            },
+            labeled: {
+                type: Boolean,
+                default: false,
+            }
+        },
+        methods: {
+            toggle() {
+                this.$emit('updateField', !this.checked);
+            }
+        },
+
+        watcher: () => ({
+            checked: () => {
+                console.log("watchers checked");
+            }
+        })
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    label {
+        @apply uppercase font-bold text-gray-500 text-sm;
+    }
+</style>
---
+++ resources/js/components/ui/UiInput.vue
@@ -0,0 +1,55 @@
+<template>
+    <input
+        :type="type"
+        :id="name"
+        :name="name"
+        class="p-2 px-3 rounded bg-gray-300 text-gray-700 outline-none placeholder-gray-500 self-center align-middle"
+        :value="value"
+        required
+        :autocomplete="type"
+        :autofocus="autofocus"
+        :placeholder="placeholder"
+    >
+</template>
+
+<script>
+    export default {
+        name: "ui-input",
+
+        props: {
+            type: {
+                type: String,
+                default: 'text'
+            },
+            name: {
+                type: String,
+                required: true,
+            },
+            value: {
+                type: String,
+                default: '',
+            },
+            autofocus: {
+                type: Boolean,
+                default: false,
+            },
+            placeholder: {
+                type: String,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    input {
+        /* variants */
+        &[labeled] {
+            @apply pt-8;
+        }
+
+        &[w-full] {
+            @apply w-full;
+        }
+    }
+</style>
