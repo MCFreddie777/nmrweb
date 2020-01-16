467681dbac0b2a387c696d6a4f95e16af4779958
František Gič <mcfreddie777@gmail.com>
2020-01-16 18:58:10 +0100

    Implement link back

---
+++ resources/js/components/ui/UiLinkBack.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <a href="#" @click.prevent="$router.go(-1)">
+        <div class="mr-6 flex items-center text-gray-500 hover:text-yellow-500">
+            <i class="fas fa-arrow-left"/>
+            <p class="ml-2">Späť</p>
+        </div>
+    </a>
+</template>
+
+<script>
+    export default {
+        name: "UiLinkBack"
+    }
+</script>
+
+<style scoped>
+
+</style>
