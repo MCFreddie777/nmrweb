f8afdf75ddee806b89773f6c36a8de9c06912a9d
František Gič <mcfreddie777@gmail.com>
2020-01-18 18:08:04 +0100

    Create basic migrations

---
+++ database/migrations/2020_01_18_170345_create_samples_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSamplesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('samples', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+
+            $table->unsignedBigInteger('solvent_id');
+            $table->boolean('own_diluent');
+            $table->decimal('amount');
+
+            $table->string('structure');
+            $table->integer('spectrometer');
+
+            $table->boolean('return');
+            $table->unsignedBigInteger('grant_id');
+
+            $table->string('note');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('solvent_id')->references('id')->on('solvents')->onDelete('cascade');
+            $table->foreign('grant_id')->references('id')->on('grants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('samples');
+    }
+}
