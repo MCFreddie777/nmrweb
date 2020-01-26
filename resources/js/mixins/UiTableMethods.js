4d4c3a5572bf9066d89f5a181ffb008e428cbffe
František Gič <mcfreddie777@gmail.com>
2020-01-26 15:55:07 +0100

    Support responsize table layout columns

--- resources/js/mixins/UiTableMethods.js
+++ resources/js/mixins/UiTableMethods.js
@@ -2,6 +2,7 @@ export default {
     tableRowsClassObject: (options, index) => {
         return [
             options.layout[index] && options.layout[index].width ? `w-${options.layout[index].width}` : '',
+            options.layout[index] && options.layout[index]['width-sm'] ? `sm:w-${options.layout[index]['width-sm']}` : '',
             {'first:pl-6 text-left justify-start': (options.layout[index] && options.layout[index].left)}
         ]
     }
